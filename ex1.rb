f0484de1bfaa23f0ab8914810abc63e46a0e5b99
ilyana19 <aries_2152@hotmail.com>
2018-07-25 17:54:30 -0400

    redo the thing in antoher way without using reduce/inject added comments

--- ex1.rb
+++ ex1.rb
@@ -5,8 +5,21 @@
 list = [1, 2, 3, 4, 5]
 
 def odd(array)
-  #find all elements that are odd numbers and then add them
-  array.find_all {|n| n % 2 == 1}.reduce(:+)
+  # find all elements that are odd numbers and then add them
+  # array.find_all {|n| n % 2 == 1}.reduce(:+)
+
+  # not gonna use reduce/inject
+  # idea is to loop through the array and find the odd number(s) in it
+  # have a "counter" variable outside of the loop to be able to access it afterwards
+  # once it's verified that it's an odd number, add that number to "counter"
+  # then return "counter"
+  counter = 0
+  array.each do |n|
+    if n % 2 == 1
+      counter += n
+    end
+  end
+  return counter
 end
 
-p odd(list)
+p odd(list) # => 9
